5fe77c1b428e42cfbf6f1933a99efbc3ee3908a6
Thomas Blondel <tfoenix@gmail.com>
2022-10-21 01:04:16 +0200

    [API] Configured swagger and updated config file

--- ShinkoAPI/Program.cs
+++ ShinkoAPI/Program.cs
@@ -1,11 +1,31 @@
+using System.Reflection;
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("1.0", new OpenApiInfo
+    {
+        Version = "1.0",
+        Title = "Restaurants API",
+        Description = "An ASP.NET Core Web API for getting free reservations on some restaurants",
+        Contact = new OpenApiContact
+        {
+            Name = "Thomas Blondel",
+            Email = "thomas.foenix-blondel@epita.fr"
+        }
+    });
+    
+    var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+});
+
+// Dependency injection
+builder.Services.AddHttpClient();
 
 var app = builder.Build();
 
@@ -13,7 +33,11 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/1.0/swagger.json", "v1");
+        options.RoutePrefix = string.Empty;
+    });
 }
 
 app.UseHttpsRedirection();
