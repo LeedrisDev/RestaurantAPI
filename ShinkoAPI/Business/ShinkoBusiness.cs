cc3f758098be1fcd2dcd72dc46c11eb3c1f9fab8
Thomas Blondel <tfoenix@gmail.com>
2022-10-21 14:15:41 +0200

    [Business] Implemented functions

--- ShinkoAPI/Business/ShinkoBusiness.cs
+++ ShinkoAPI/Business/ShinkoBusiness.cs
@@ -1,11 +1,24 @@
+using ShinkoAPI.DataAccess;
+
 namespace ShinkoAPI.Business;
 
 /// <inheritdoc />
 public class ShinkoBusiness: IShinkoBusiness
 {
+    private readonly IShinkoData _shinkoData;
+    
+    /// <summary>
+    /// Constructor
+    /// </summary>
+    /// <param name="shinkoData">Data Access layer for Shinko API</param>
+    public ShinkoBusiness(IShinkoData shinkoData)
+    {
+        _shinkoData = shinkoData;
+    }
+    
     /// <inheritdoc />
     public async Task<IList<DateTime>> GetAvailableReservations(DateTime beginDate, DateTime endDate, int nbGuests, bool isLunch, bool isDinner)
     {
-        throw new NotImplementedException();
+        return await _shinkoData.GetAvailableReservations(beginDate, endDate, nbGuests, isLunch, isDinner);
     }
 }
